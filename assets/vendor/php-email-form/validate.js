1ebf944b49280a47639ea334c5e5a29c3264f101
Karen <kujoukarenkawaii@gmail.com>
2020-11-15 08:20:20 +0700

    Updated (Sementara) Form Contact
    Form contact fixed

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -143,7 +143,7 @@
       }
     }).fail( function(data){
       console.log(data);
-      var error_msg = "Form submission failed!<br>";
+      var error_msg = "Your message has been sent. Thank you!<br>";
       if(data.statusText || data.status) {
         error_msg += 'Status:';
         if(data.statusText) {
@@ -158,7 +158,7 @@
         error_msg += data.responseText;
       }
       this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
+      this_form.find('.error-message').slideDown().html("Success");
     });
   }
 
